002b3ad6d666f114f17f8407fcb0d0edc4d5f8c5
Malikic1 <azeezmalik19@gmail.com>
2023-10-20 01:58:36 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      esm: "405px",
+      sm: "480px",
+      es: "605px",
+      md: "768px",
+      emd: "900px",
+      lg: "976px",
+      xl: "1300px",
+    },
+    extend: {
+      colors: {
+        black: "black",
+        light: "white",
+        smoke: " rgb(245, 245, 245)",
+        dark: " #1c1c1c",
+        gray: " rgb(86, 86, 86)",
+        grey: "#acacac",
+        trans: "rgba(25, 25, 25, 0.522)",
+        blue: "#3371f2",
+      },
+    },
+  },
+  plugins: [],
+};
